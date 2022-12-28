bb3e8dfa3e24b5a8e85dded359d3f811dc91bb93
Oleg <olegkuz39@gmail.com>
2022-12-28 17:35:54 +0200

    Переменные

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         int nextInt= 10;//деклариция с инициализацией
         short myShort = 32323; //16-бит инфы
         long myLong =784684333; // 64-бита инфы
+        double myDouble = 738382.32; //64-бита инфы
+        float myFloat = 3213.4f; // 32-бита инфы добавить "f" при дробных числах
         System.out.println(myInt+nextInt+myShort+myLong); //метод с параметром в виде имени переменной
     }
 }
