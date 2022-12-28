197297888a0baad01dad13130fd56cdd51671257
Oleg <olegkuz39@gmail.com>
2022-12-28 16:54:47 +0200

    Переменные

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int myInt;//декларирование(объявление) переменной
+        int myInt;//декларирование(объявление) числовой переменной переменной (32-битные числа)
         myInt=557;//инициализация переменной
         int nextInt= 10;//деклариция с инициализацией
         System.out.println(myInt+nextInt); //метод с параметром в виде имени переменной
